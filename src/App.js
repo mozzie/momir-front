05e8b9e287a055b85a4facbd66b0878226e5d056
Jussi Timonen <jussi.timonen@houston-inc.com>
2020-04-03 16:50:39 +0300

    Removes printers

--- src/App.js
+++ src/App.js
@@ -11,18 +11,9 @@ const App = () => {
   const [printable, setPrintable] = useState(false);
   const [printer, setPrinter] = useState(undefined);
   const [printerList, setPrinterList] = useState([]);
-
+ 
   const backend = process.env.REACT_APP_BACKEND_URL;
 
-  useEffect(() => {
-    const getPrinters = async () => {
-      let printers = await axios.get(backend + "printers", {
-        headers: { "Access-Control-Allow-Origin": "*" }
-      });
-      setPrinterList(printers.data);
-    };
-    getPrinters();
-  }, []);
   const getCard = async c => {
     setLoading(true);
     const result = await axios.get(backend + "card/" + c, {
@@ -35,42 +26,14 @@ const App = () => {
     if (card.scryfallId) {
       setImageUrl(backend + "picture/" + card.scryfallId);
       setLoading(false);
-      setPrintable(true);
     }
   }, [card]);
-
-  const print = card => {
-    axios.get(backend + "print/" + card.scryfallId, {
-      params: { printer: printer },
-      headers: { "Access-Control-Allow-Origin": "*" }
-    });
-    setPrintable(false);
-  };
 
   const increaseCount = () => setCount(Math.min(16, count + 1));
   const decreaseCount = () => setCount(Math.max(0, count - 1));
 
-  const handlePrinterChange = event => setPrinter(event.target.value);
-
   return (
     <div>
-      <p>
-       {printerList.length > 0 && (
-        <Select
-          onChange={handlePrinterChange}
-          value={printer}
-        >
-          <option key="none" value="">
-            None
-          </option>
-          {printerList.map(i => (
-            <option key={i.name} value={i.name}>
-              {i.name}
-            </option>
-          ))}
-        </Select>
-      )}
-      </p>
       <p>
         <Button onClick={() => decreaseCount()}
           disabled={count < 1 || loading}
@@ -95,14 +58,6 @@ const App = () => {
           variant="outlined"
         >
           Activate
-        </Button>
-        {printer && (
-        <Button onClick={() => print(card)}
-        disabled={!printable}
-        variant="outlined"
-        >
-          Print
-        </Button>
       )}
       </p>
       <img src={imageUrl} alt={card ? card.name : "no card"} />
